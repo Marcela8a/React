d1c9b4a254de3ba7efc93a28f02eedaa6725497b
DESKTOP-A4FT4GU\USUARIO <mochoa@mbp.com.co>
2023-05-29 20:37:12 -0500

    se ajusta archivo para agregar children

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import './App.css'
-import Card from './components/Card'
+import React, { useState } from 'react';
+import './App.css';
+import Card from './components/Card';
 
 function App() {
-  
+  const [nombre, setNombre] = useState('');
+  const [formulario, setFormulario] = useState(false);
+
+  const pepito = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (nombre === '') {
+      alert('Esto está mal');
+    } else {
+      setFormulario(true);
+      alert('Formulario OK');
+    }
+  };
 
   return (
     <>
-    <div className='App'>
-    <h1>Carga de Estudiantes</h1>
-    <form> </form>
-     <Card/>
-     </div>
+      <div className='App'>
+        <h1>Carga de Estudiantes</h1>
+        <form onSubmit={handleSubmit}>
+          <label></label>
+          <input value={nombre} onChange={pepito} />
+          <input type='submit' />
+        </form>
+        <Card>
+        <h2>Título del Card</h2>
+          <p>Contenido adicional dentro del Card</p>
+        </Card>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Card = () => {
+const Card = ({ children }) => {
   return (
-    <div>Hola, Soy una Tarjeta</div>
-  )
-}
+    <div className='Card'>
+      <div className='Card-header'>
+        <h2>Título de la tarjeta</h2>
+      </div>
+      <div className='Card-body'>
+        {children}
+      </div>
+      <div className='Card-footer'>
+        <p>Texto del pie de página</p>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
